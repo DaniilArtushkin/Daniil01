829144fd4254ac45e07ce2a502d21518cf53fb00
example@gmail.com <exzemple@gmail.com>
2020-09-06 15:26:16 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+console.log(4/0);
+console.log("Hello")
+ 
+let persone = {
+    name:"john",
+    age: 25,
+    isMarried:false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plun.png','orange.jpg','apple.bmp'];
+
+console.log(arr[2]);
+
+//alert("Hello World");
+
+//let anser = confirm("Arr you here?");
+
+//console.log(anser);
+
+//let anser = +prompt("Есть ли вам 18?","Да");
+
+//console.log(typeof(anser));
+//console.log("arr"+"- object");
+//console.log( 4 +"- object");
+
+let incr = 10,
+    decr = 10;
+  
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+let isChecet = true,
+    isClose = true;
+
+console.log(isChecet || !isClose);
